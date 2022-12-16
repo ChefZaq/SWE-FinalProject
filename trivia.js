e5f27c03ada3e5a225d1a6ba541e21e4d5880fb0
abdoulbah418 <69488874+abdoulbah418@users.noreply.github.com>
2022-12-15 23:39:49 -0500

    Project

---
+++ trivia.js
@@ -0,0 +1,114 @@
+let questions = [
+    {
+    numb: 1,
+    question: "What is the most points scored in a game?",
+    answer: "100",
+    options: [
+      "84",
+      "92",
+      "100",
+      "110"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Which of these players were not drafted in '03?",
+    answer: "Dwight Howard",
+    options: [
+      "LeBron James",
+      "Dwayne Wade",
+      "Carmelo Anthony",
+      "Dwight Howard"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What team has the best NBA regular season record?",
+    answer: "2015 - 2016 Golden State Warriors",
+    options: [
+      "2014 - 2015 Golden State Warriors",
+      "1995 - 1996 Chicago Bulls",
+      "2015 - 2016 Golden State Warriors",
+      "2000 - 2001 Los Angeles Lakers"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Who has the most NBA Championship Rings?",
+    answer: "Bill Russsel",
+    options: [
+      "LeBron James ",
+      "Kobe Bryant",
+      "Michael Jordan",
+      "Bill Russell"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Who holds the record for most turnovers in NBA History?",
+    answer: "LeBron James",
+    options: [
+      "James Harden",
+      "Karl Malone",
+      "Russell Westbrook",
+      "LeBron James"
+    ]
+  },
+    {
+    numb: 6,
+    question: "What NBA Franchise that's never moved cites?",
+    answer: "New York Knicks",
+    options: [
+      "New York Knicks",
+      "Los Angeles Lakers",
+      "Oklahoma City Thunder",
+      "Toronto Rapters"
+    ]
+  },
+    {
+    numb: 7,
+    question: "Which team has the most consecutive championship wins?",
+    answer: "Boston Celtics",
+    options: [
+      "Golden State Warriors",
+      "Miami Heat",
+      "Chicago Bulls",
+      "Boston Celtics"
+    ]
+  },
+    {
+    numb: 8,
+    question: "Which of these Hawks players weren't in the 2015 All Star Game?",
+    answer: "Dennis Schroder",
+    options: [
+      "Paul Millsap",
+      "Jeff Teague",
+      "Dennis Schroder",
+      "Al Horford"
+    ]
+  },
+    {
+    numb: 9,
+    question: "Who was the younger player to be NBA MVP?",
+    answer: "Derrick Rose",
+    options: [
+      "Kobe Bryant",
+      "LeBron James",
+      "Derrick Rose",
+      "Stephen Curry"
+    ]
+  },
+    {
+    numb: 10,
+    question: "Who is the Greatest Player of All Time?",
+    answer: "Michael Jordan",
+    options: [
+      "Kobe Bryant",
+      "LeBron James",
+      "Michael Jordan",
+      "Shaquille O'Neal"
+    ]
+  },
+
+ 
+];
